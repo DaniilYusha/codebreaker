e7fc4bd9fcb9c1ae6298c76c4cef106f499c1793
DaniilYusha <ilushenko79@gmail.com>
2020-07-08 14:40:16 +0300

    Add validate method to User class

--- lib/codebreaker/entities/user.rb
+++ lib/codebreaker/entities/user.rb
@@ -1,9 +1,28 @@
+# frozen_string_literal: true
+
 module Codebreaker
-
+  # User class contains user name field
   class User
+    attr_reader :name
 
+    NAME_MIN_LENGTH = 3
+    NAME_MAX_LENGTH = 20
 
+    def initialize(name)
+      validate name
+      @name = name
+    end
 
+    private
+
+    def validate_name(name)
+      raise NameIsNotStringError unless name.is_a? String
+      raise ShortNameError, "Name should be no less than #{NAME_MIN_LENGTH} symbols" if name.length < NAME_MIN_LENGTH
+      raise LongNameError, "Name should be no more than #{NAME_MAX_LENGTH} symbols" if name.length > NAME_MAX_LENGTH
+    end
+
+    def validate(name)
+      validate_name name
+    end
   end
-
 end
