7e896c2ac51bb2823296f6857309e6621ac7f213
DaniilYusha <ilushenko79@gmail.com>
2020-07-08 14:41:57 +0300

    Add take_hint, check_attempt and validation methods to Game class

--- lib/codebreaker/entities/game.rb
+++ lib/codebreaker/entities/game.rb
@@ -1,9 +1,63 @@
+# frozen_string_literal: true
+
 module Codebreaker
-
+  # Game class contains game scripts
   class Game
+    include Validator
+    attr_reader :user, :difficulty, :secret_code, :hints_list
 
-    
+    WIN = :win
+    LOSE = :lose
 
+    def initialize(user, difficulty)
+      validate user, difficulty
+
+      @user = user
+      @difficulty = difficulty
+      @secret_code = Core.new.generate_secret_code
+      @hints_list = @secret_code.clone
+    end
+
+    def take_hint
+      return nil if @difficulty.hints_num.zero?
+
+      @difficulty.hints_num -= 1
+      index = rand(@hints_list.size)
+      digit = @hints_list[index]
+      @hints_list.delete_at index
+      digit
+    end
+
+    def check_attempt(guess)
+      return LOSE if lose?
+
+      user_code_array = guess.split('').map(&:to_i)
+      validate_guess user_code_array
+      @difficulty.attempts_num -= 1
+      return WIN if win? user_code_array
+
+      @secret_code_clone = @secret_code.clone
+      Core.new.calculate_matches @secret_code_clone, user_code_array
+    end
+
+    private
+
+    def validate_guess(guess_array)
+      raise DigitsCountError unless guess_array.count == Core::DIGITS_NUM
+      raise DigitRangeError unless guess_array.all? { |digit| digit.between? Core::MIN_CODE_NUM, Core::MAX_CODE_NUM }
+    end
+
+    def validate(user, difficulty)
+      compare_classes user.class, User
+      compare_classes difficulty.class, Difficulty
+    end
+
+    def lose?
+      @difficulty.attempts_num.zero?
+    end
+
+    def win?(user_code_array)
+      user_code_array == @secret_code
+    end
   end
-
 end
