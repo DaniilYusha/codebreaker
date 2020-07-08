e5808e9c7f74778e3f985652567995948519581d
DaniilYusha <ilushenko79@gmail.com>
2020-07-08 17:08:52 +0300

    Rename file statistics_handler.rb to file_loader.rb

---
+++ lib/codebreaker/modules/file_loader.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Codebreaker
+  # Module FileLoader contains methods for data saving and loading
+  module FileLoader
+    FILE_NAME = 'statistics.yml'
+
+    def save(data)
+
+    end
+  end
+end
--- lib/codebreaker/modules/statistics_handler.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Codebreaker
-  # Module StatisticsHandler contains methods for data saving and loading
-  module StatisticsHandler; end
-end
