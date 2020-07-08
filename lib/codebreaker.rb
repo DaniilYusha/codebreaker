33ec0166e1d50b90f3599fc6225fece7d62bd2a9
DaniilYusha <ilushenko79@gmail.com>
2020-07-08 14:46:29 +0300

    Add connected files to codebreaker.rb

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,6 +1,17 @@
-require "codebreaker/version"
+# frozen_string_literal: true
 
-module Codebreaker
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require_relative 'codebreaker/version'
+
+require_relative 'codebreaker/errors/name_is_not_string_error'
+require_relative 'codebreaker/errors/short_name_error'
+require_relative 'codebreaker/errors/long_name_error'
+require_relative 'codebreaker/errors/difficulty_error'
+require_relative 'codebreaker/errors/digit_range_error'
+require_relative 'codebreaker/errors/digits_count_error'
+
+require_relative 'codebreaker/modules/validator'
+
+require_relative 'codebreaker/entities/difficulty'
+require_relative 'codebreaker/entities/core'
+require_relative 'codebreaker/entities/game'
+require_relative 'codebreaker/entities/user'
