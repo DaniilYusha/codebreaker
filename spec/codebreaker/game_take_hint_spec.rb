5a10cacac733e5e54bd45c469b18d0e96c0793ee
DaniilYusha <ilushenko79@gmail.com>
2020-07-13 14:19:50 +0300

    Test take_hint method

---
+++ spec/codebreaker/game_take_hint_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+RSpec.describe Codebreaker::Game do
+  let(:user) { Codebreaker::User.new 'Daniil' }
+  let(:difficulty) { Codebreaker::Difficulty.new 'hell' }
+  let(:game) { described_class.new user, difficulty }
+
+  describe '#take_hint' do
+    it 'returns nil when hints count equal to zero' do
+      difficulty.instance_variable_set(:@current_hints, 0)
+      expect(game.take_hint).to eq nil
+    end
+
+    it 'returns Integer' do
+      expect(game.take_hint.class).to eq Integer
+    end
+
+    it 'returns number from secret code' do
+      expect(game.secret_code).to include game.take_hint
+    end
+
+    it 'returns only one number' do
+      expect(game.take_hint.to_s.size).to eq 1
+    end
+
+    it 'changes current_hints counter by 1' do
+      expect { game.take_hint }.to change { difficulty.instance_variable_get(:@current_hints) }.by(-1)
+    end
+
+    it 'removes one element from hints list' do
+      expect { game.take_hint }.to change { game.instance_variable_get(:@hints_list).size }.by(-1)
+    end
+  end
+end
