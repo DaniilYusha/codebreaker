da7a77db138db3e73fc424bff73fa4793f47d07a
DaniilYusha <ilushenko79@gmail.com>
2020-07-14 21:18:38 +0300

    Add errors instance variable and change validate_difficulty method

--- lib/codebreaker/entities/difficulty.rb
+++ lib/codebreaker/entities/difficulty.rb
@@ -3,7 +3,7 @@
 module Codebreaker
   # Difficulty class contains list of game difficulties
   class Difficulty
-    attr_reader :kind, :attempts, :hints
+    attr_reader :kind, :attempts, :hints, :errors
     attr_accessor :current_attempts, :current_hints
 
     LIST = { easy: { attempts: 15, hints: 2 },
@@ -11,18 +11,18 @@ module Codebreaker
              hell: { attempts: 5, hints: 1 } }.freeze
 
     def initialize(difficulty)
-      validate difficulty
       @kind = difficulty.to_sym
       @attempts = LIST[@kind][:attempts]
       @current_attempts = @attempts.clone
       @hints = LIST[@kind][:hints]
       @current_hints = @hints.clone
+      @errors = []
     end
 
     private
 
-    def validate_difficulty(difficulty)
-      raise DifficultyError unless LIST.include? difficulty.to_sym
+    def validate_difficulty
+      @errors << DifficultyError unless LIST.include? @kind
     end
 
     def validate(difficulty)
