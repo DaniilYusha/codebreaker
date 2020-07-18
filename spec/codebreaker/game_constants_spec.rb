af045d07f5fa4a936ed3ccf5d9b251094252cbc0
DaniilYusha <ilushenko79@gmail.com>
2020-07-18 17:57:46 +0300

    Add changes to tests for Game class

--- spec/codebreaker/game_constants_spec.rb
+++ spec/codebreaker/game_constants_spec.rb
@@ -2,6 +2,17 @@
 
 RSpec.describe Codebreaker::Game do
   describe 'check Game constants' do
+    it 'check MIN_CODE_NUM constant' do
+      expect(described_class::MIN_CODE_NUM).to eq 1
+    end
+
+    it 'check MAX_CODE_NUM constant' do
+      expect(described_class::MAX_CODE_NUM).to eq 6
+    end
+
+    it 'check DIGITS_NUM constant' do
+      expect(described_class::DIGITS_NUM).to eq 4
+    end
     it 'check WIN constant' do
       expect(described_class::WIN).to eq :win
     end
--- spec/codebreaker/game_init_spec.rb
+++ spec/codebreaker/game_init_spec.rb
@@ -5,20 +5,30 @@ RSpec.describe Codebreaker::Game do
   let(:difficulty) { Codebreaker::Difficulty.new 'hell' }
   let(:game) { described_class.new user, difficulty }
   describe '.init' do
-    it 'saves a secret code' do
-      expect(game.instance_variable_get(:@secret_code)).not_to be_empty
+    it 'secret code is an Array' do
+      expect(game.secret_code.class).to eq Array
+    end
+
+    it 'secret code size equal to DIGITS_NUM constant' do
+      expect(game.secret_code.size).to eq described_class::DIGITS_NUM
+    end
+
+    it 'each element of secret code is between MIN_CODE_NUM and MAX_CODE_NUM contstants' do
+      game.secret_code.each do |digit|
+        expect(digit).to be_between(described_class::MIN_CODE_NUM, described_class::MAX_CODE_NUM).inclusive
+      end
     end
 
     it 'hints list is equal to secret code' do
-      expect(game.instance_variable_get(:@secret_code)).to eq game.instance_variable_get(:@hints_list)
+      expect(game.hints_list).to eq game.secret_code
     end
 
     it 'user field class equal to User' do
-      expect(game.instance_variable_get(:@user).class).to eq Codebreaker::User
+      expect(game.user.class).to eq Codebreaker::User
     end
 
     it 'difficulty field class equal to Difficulty' do
-      expect(game.instance_variable_get(:@difficulty).class).to eq Codebreaker::Difficulty
+      expect(game.difficulty.class).to eq Codebreaker::Difficulty
     end
   end
 end
