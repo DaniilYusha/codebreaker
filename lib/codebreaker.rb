94b933f389d5a1a0cc3cd91818fd9041a6418f9b
DaniilYusha <ilushenko79@gmail.com>
2020-07-18 17:26:06 +0300

    Change some requirements

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -13,10 +13,11 @@ require_relative 'codebreaker/errors/digits_count_error'
 require_relative 'codebreaker/errors/expected_difficulty_instance_error'
 require_relative 'codebreaker/errors/expected_user_instance_error'
 
-require_relative 'codebreaker/modules/core'
 require_relative 'codebreaker/modules/validator'
-require_relative 'codebreaker/modules/statistics_loader'
+
+require_relative 'codebreaker/services/statistics_service'
 
 require_relative 'codebreaker/entities/difficulty'
 require_relative 'codebreaker/entities/game'
+require_relative 'codebreaker/entities/guess_checker'
 require_relative 'codebreaker/entities/user'
