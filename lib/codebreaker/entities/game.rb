36f703be4b59f3b297e30cf6a4edc1cb77b66b51
DaniilYusha <ilushenko79@gmail.com>
2020-07-18 17:16:19 +0300

    Add improvements to check_attempt method and move some logic

--- lib/codebreaker/entities/game.rb
+++ lib/codebreaker/entities/game.rb
@@ -3,10 +3,11 @@
 module Codebreaker
   class Game
     include Validator
-    include StatisticsLoader
-    include Core
     attr_reader :user, :difficulty, :secret_code, :hints_list, :errors
 
+    MIN_CODE_NUM = 1
+    MAX_CODE_NUM = 6
+    DIGITS_NUM = 4
     WIN = :win
     LOSE = :lose
 
@@ -14,58 +15,54 @@ module Codebreaker
       @user = user
       @difficulty = difficulty
       @secret_code = generate_secret_code
-      @hints_list = @secret_code.clone
+      @hints_list = secret_code.clone
       @errors = []
     end
 
     def take_hint
-      return nil if @difficulty.current_hints.zero?
+      return nil if difficulty.current_hints.zero?
 
-      @difficulty.current_hints -= 1
-      index = rand(@hints_list.size)
-      digit = @hints_list[index]
-      @hints_list.delete_at index
+      difficulty.current_hints -= 1
+      index = rand(hints_list.size)
+      digit = hints_list[index]
+      hints_list.delete_at index
       digit
     end
 
     def check_attempt(guess)
       return LOSE if lose?
 
-      user_code_array = guess.split('').map(&:to_i)
-      validate_guess user_code_array
-      @difficulty.current_attempts -= 1
-      return WIN if win? user_code_array
+      difficulty.current_attempts -= 1
+      return WIN if win? guess
 
-      @secret_code_copy = @secret_code.clone
-      calculate_matches user_code_array
+      GuessChecker.new(secret_code.clone, guess).check
     end
 
     private
 
-    def validate_guess(guess_array)
-      raise DigitsCountError unless guess_array.count == DIGITS_NUM
-      raise DigitRangeError unless guess_array.all? { |digit| digit.between? MIN_CODE_NUM, MAX_CODE_NUM }
+    def generate_secret_code
+      DIGITS_NUM.times.map { rand(MIN_CODE_NUM..MAX_CODE_NUM) }
     end
 
     def lose?
-      @difficulty.current_attempts.zero?
+      difficulty.current_attempts.zero?
     end
 
-    def win?(user_code_array)
-      user_code_array == @secret_code
+    def win?(user_code)
+      user_code.to_i == secret_code.join
     end
 
     def validate!
       validate_user
-      validate_difficulty if @errors.empty?
+      validate_difficulty if errors.empty?
     end
 
     def validate_user
-      @errors << ExpectedUserInstanceError unless @user.class == Codebreaker::User
+      errors << ExpectedUserInstanceError unless user.class == Codebreaker::User
     end
 
     def validate_difficulty
-      @errors << ExpectedDifficultyInstanceError unless @difficulty.class == Codebreaker::Difficulty
+      errors << ExpectedDifficultyInstanceError unless difficulty.class == Codebreaker::Difficulty
     end
   end
 end
