e477a5b7a2fa715d52838a13f96cca4eb36d37d4
DaniilYusha <ilushenko79@gmail.com>
2020-07-21 13:40:06 +0300

    Add sort_statistics method

--- lib/codebreaker/services/statistics_service.rb
+++ lib/codebreaker/services/statistics_service.rb
@@ -4,17 +4,20 @@ module Codebreaker
   class StatisticsService
     attr_reader :game, :path
 
-    def initialize(game, path)
-      @game = game
+    def initialize(path)
       @path = path
     end
 
-    def store
+    def store(game)
       statistics = File.exist?(path) && !File.zero?(path) ? YAML.load_file(path) : []
-      statistics << pack_game_data
+      statistics << pack_game_data(game)
       file = File.open(path, 'w')
       file.write(statistics.to_yaml)
       file.close
+    end
+
+    def sort_statistics
+      load.sort_by { |user| [user[:attempts_total], user[:attempts_used], user[:hints_used]] }
     end
 
     def load
@@ -25,7 +28,7 @@ module Codebreaker
 
     private
 
-    def pack_game_data
+    def pack_game_data(game)
       {
         name: game.user.name,
         difficulty: game.difficulty.kind.to_s,
